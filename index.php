23a4211eafac0655a069d295e0b1be9180a10e6d
Mifist <avanturka1992@gmail.com>
2017-10-05 09:13:19 +0300

    Add home files at work

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php header ("Content-Type: text/html; charset=utf-8"); ?>
+
+<!-- START Head -->
+<?php include_once 'templates/head.php'; ?>
+<!-- END Head -->
+
+<!-- START Header -->
+<?php include_once 'templates/header.php'; ?>
+<!-- END Header -->
+
+<!-- START Home content -->
+<?php include_once 'templates/home.php'; ?>
+<!-- ENDHome content -->
+
+<!-- START Footer -->
+	<?php include_once 'templates/footer.php'; ?>
+<!-- END Footer -->
---
+++ templates/home.php
@@ -0,0 +1,223 @@
+<!-- START Main container -->
+<main>
+	<h1>home page</h1>
+	
+	<!-- START TEXT Block -->
+	<div class="container">
+		<div class="text-block">
+			<h2 class="section-title white">немного истории</h2>
+			<p>Компания TASOMA промо текст <br>
+				Собственное производство в России и за рубежом, позволяет нам, минуя посредников, напрямую поставлять продукцию на наши склады, что существенно снижает стоимость комплектующих без потери качества.</p>
+			<p>Работаем с торговыми  и строительными компаниями по всей России <br>
+				Собственное производство в России и за рубежом, позволяет нам, минуя посредниковсклады, что существенно снижает стоимость комплектующих без потери качества.</p>
+		</div>
+	</div><!-- END TEXT Block -->
+	
+	<!-- START View Catalog -->
+	<div class="view-catalog">
+		<div class="container view-title">
+			<div class="text-description">
+				<p>География продаж насчитывает 20 регионов <br>
+					и более 5 крупных предприятий Российской <br>
+					Федерации</p>
+			</div>
+			<h2 class="section-title">продаем оборудование в самые удаленные уголки страны</h2>
+		</div>
+		<div class="container-fluid">
+			<div class="row view-row">
+				<div class="col-lg-7 view-img" style="background-image: url('assets/images/IMG_7546.jpg')"></div>
+				<div class="col-lg-5 view-text">
+					<ul class="catalog-description">
+						<li>Скидки после первого заказа</li>
+						<li>Возможность бесплатной доставки</li>
+						<li>база данных документации</li>
+						<li>выезд к инженерам</li>
+						<li>расчет нестандартных решений</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	
+	</div>
+	<!-- ENDView Catalog -->
+	
+	<!-- START TEXT Block -->
+	<div class="container">
+		<div class="text-block">
+			<h2 class="section-title white">собственное  производство</h2>
+			<p>Компания TASOMA промо текст <br>
+				Собственное производство в России и за рубежом, позволяет нам, минуя посредников, напрямую поставлять продукцию на наши склады, что существенно снижает стоимость комплектующих без потери качества.</p>
+			<p>Работаем с торговыми  и строительными компаниями по всей России <br>
+				Собственное производство в России и за рубежом, позволяет нам, минуя посредниковсклады, что существенно снижает стоимость комплектующих без потери качества.</p>
+		</div>
+	</div><!-- END TEXT Block -->
+	
+	<!-- START Gallery -->
+	<div class="gallery">
+		<div class="container">
+			<h2 class="section-title">галлерея нашего производства</h2>
+			
+			<div id="gallery" class="slick-slider">
+				
+				<div class="slick-slide">
+					<div class="gallery-img">
+						<img src="assets/images/IMG_2492.jpg" alt="Slide 1">
+					</div>
+				</div> <!-- end slide-->
+				<div class="slick-slide">
+					<div class="gallery-img">
+						<img src="assets/images/IMG_2585.jpg" alt="Slide 2">
+					</div>
+				</div> <!-- end slide-->
+				<div class="slick-slide">
+					<div class="gallery-img">
+						<img src="assets/images/IMG_2612.jpg" alt="Slide 3">
+					</div>
+				</div> <!-- end slide-->
+				<div class="slick-slide">
+					<div class="gallery-img">
+						<img src="assets/images/IMG_7288.jpg" alt="Slide 4">
+					</div>
+				</div> <!-- end slide-->
+				<div class="slick-slide">
+					<div class="gallery-img">
+						<img src="assets/images/IMG_7299.jpg" alt="Slide 5">
+					</div>
+				</div> <!-- end slide-->
+			
+			</div><!-- END of  #home-slider-->
+		</div>
+	</div><!-- END Gallery -->
+	
+	<!-- START Geography -->
+	<div class="geography">
+		<div class="container">
+			<h2 class="white section-title">Георафия продаж <br> наших щитов</h2>
+			<img class="geography-img" src="assets/images/map.png" alt="Георафия продаж наших щитов">
+			<div class="text-description white">
+				<p>Расширенная география продаж и отлаженная логистика позволяет направлять Вам <br>
+					заказы в максимально короткие сроки</p>
+			</div>
+		
+		</div>
+	</div><!-- END Geography -->
+	
+	<!-- START Last Works -->
+	<div class="last-works">
+		<div class="container">
+			<div class="text-description">
+				<p>Предлагаем Вам как стандартные изделия, так и <br>
+					нестандартное оборудование, выполненное по<br>
+					Вашему заданию</p>
+			</div>
+			
+			<h2 class="section-title">Последние Выполненные работы</h2>
+			
+			<div id="last-works-slider" class="slick-slider">
+				<div class="slick-slide">
+					<div class="slide-wrap">
+						<div class="slide-img">
+							<img src="assets/images/vegas.jpg" alt="Slide">
+						</div>
+						<div class="slider-caption">
+							<div class="touch-title fa fa-id-card-o" aria-hidden="true">
+								<p>Vegas City HalL</p>
+							</div>
+							<div class="touch-description" >
+								<p>Vegas City Hall (ТРК VEGAS Крокус Сити), г. Красногорск, Московская область.</p>
+							</div>
+							<div class="touch-title fa fa-file-text-o" aria-hidden="true">
+								<p>шкафы управления насосами</p>
+							</div>
+							<div class="touch-description">
+								<p>Разработка и поставка шкафов управления дренажными насосами для КНС.</p>
+							</div>
+							<div class="touch-title fa fa-calendar-o" aria-hidden="true">
+								<p>2017</p>
+							</div>
+						
+						</div>
+					</div>
+				</div> <!-- end slide-->
+				<div class="slick-slide">
+					<div class="slide-wrap">
+						<div class="slide-img">
+							<img src="assets/images/new-york.jpg" alt="Slide">
+						</div>
+						<div class="slider-caption">
+							<div class="touch-title fa fa-id-card-o" aria-hidden="true">
+								<p>Vegas City HalL</p>
+							</div>
+							<div class="touch-description" >
+								<p>Vegas City Hall (ТРК VEGAS Крокус Сити), г. Красногорск, Московская область.</p>
+							</div>
+							<div class="touch-title fa fa-file-text-o" aria-hidden="true">
+								<p>шкафы управления насосами</p>
+							</div>
+							<div class="touch-description">
+								<p>Разработка и поставка шкафов управления дренажными насосами для КНС.</p>
+							</div>
+							<div class="touch-title fa fa-calendar-o" aria-hidden="true">
+								<p>2017</p>
+							</div>
+						
+						</div>
+					</div>
+				</div> <!-- end slide-->
+				<div class="slick-slide">
+					<div class="slide-wrap">
+						<div class="slide-img">
+							<img src="assets/images/vegas.jpg" alt="Slide">
+						</div>
+						<div class="slider-caption">
+							<div class="touch-title fa fa-id-card-o" aria-hidden="true">
+								<p>Vegas City HalL</p>
+							</div>
+							<div class="touch-description" >
+								<p>Vegas City Hall (ТРК VEGAS Крокус Сити), г. Красногорск, Московская область.</p>
+							</div>
+							<div class="touch-title fa fa-file-text-o" aria-hidden="true">
+								<p>шкафы управления насосами</p>
+							</div>
+							<div class="touch-description">
+								<p>Разработка и поставка шкафов управления дренажными насосами для КНС.</p>
+							</div>
+							<div class="touch-title fa fa-calendar-o" aria-hidden="true">
+								<p>2017</p>
+							</div>
+						
+						</div>
+					</div>
+				</div> <!-- end slide-->
+				<div class="slick-slide">
+					<div class="slide-wrap">
+						<div class="slide-img">
+							<img src="assets/images/new-york.jpg" alt="Slide">
+						</div>
+						<div class="slider-caption">
+							<div class="touch-title fa fa-id-card-o" aria-hidden="true">
+								<p>Vegas City HalL</p>
+							</div>
+							<div class="touch-description" >
+								<p>Vegas City Hall (ТРК VEGAS Крокус Сити), г. Красногорск, Московская область.</p>
+							</div>
+							<div class="touch-title fa fa-file-text-o" aria-hidden="true">
+								<p>шкафы управления насосами</p>
+							</div>
+							<div class="touch-description">
+								<p>Разработка и поставка шкафов управления дренажными насосами для КНС.</p>
+							</div>
+							<div class="touch-title fa fa-calendar-o" aria-hidden="true">
+								<p>2017</p>
+							</div>
+						
+						</div>
+					</div>
+				</div> <!-- end slide-->
+			</div><!-- END of  #home-slider-->
+		</div>
+	</div><!-- END Last Works -->
+
+
+</main>
+<!-- END Main container -->
